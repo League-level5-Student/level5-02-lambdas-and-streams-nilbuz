3f0cca27b31fe49b0737e91692f1b334263ec111
League Student <league@iMac-94.attlocal.net>
2020-03-06 16:56:24 -0800

    finished lambda methods started the stream stuff

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,5 +1,7 @@
 package _01_Lambda_Methods;
 
+import java.util.Random;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
@@ -10,31 +12,81 @@ public class LambdaMethods {
 			for (int i = 0; i < 10; i++) {
 				System.out.println(s);
 			}
+
+			System.out.println();
+
 		}, "repeat");
 
 		// 2. Call the printCustonMessage method using a lambda so that the String
 		// prints backwards.
 
-		String x = "backwards";
-		
 		printCustomMessage((s) -> {
 
-			for (int i = x.length()-1; i > -1; i--) {
+			for (int i = s.length() - 1; i > -1; i--) {
 
-				System.out.print(x.charAt(i));
-				
+				System.out.print(s.charAt(i));
+
 			}
 
-		}, x);
+			System.out.println();
+			System.out.println();
+		}, "backwards");
 
 		// 3. Call the printCustonMessage method using a lambda so that the String
 		// prints with a mix between upper an lower case characters.
 
+		printCustomMessage((s) -> {
+
+			Random random = new Random();
+
+			for (int i = 0; i < s.length(); i++) {
+
+				if (random.nextInt(2) == 0) {
+					System.out.print(Character.toUpperCase(s.charAt(i)));
+				} else {
+					System.out.print(Character.toLowerCase(s.charAt(i)));
+				}
+
+			}
+
+			System.out.println();
+			System.out.println();
+
+		}, "upperlowercase");
+
 		// 4. Call the printCustonMessage method using a lambda so that the String
 		// prints with a period in between each character.
 
+		printCustomMessage((s) -> {
+
+			for (int i = 0; i < s.length(); i++) {
+
+				System.out.print(s.charAt(i));
+				System.out.print(".");
+			}
+
+			System.out.println();
+			System.out.println();
+
+		}, "periods");
+
 		// 5. Call the printCustonMessage method using a lambda so that the String
 		// prints without any vowels.
+
+		printCustomMessage((s) -> {
+
+			s = s.replace("a", "");
+			s = s.replace("e", "");
+			s = s.replace("i", "");
+			s = s.replace("o", "");
+			s = s.replace("u", "");
+
+			System.out.print(s);
+
+			System.out.println();
+			System.out.println();
+
+		}, "onomatopoeia");
 
 	}
 
--- src/_02_Creating_Streams/CreatingStreams.java
+++ src/_02_Creating_Streams/CreatingStreams.java
@@ -2,6 +2,7 @@ package _02_Creating_Streams;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Stack;
 import java.util.stream.Stream;
@@ -9,7 +10,12 @@ import java.util.stream.Stream;
 public class CreatingStreams {
 	public static void main(String[] args) {
 		//1. Create a stream out of the following collections.
+		
+		
+		
 		String[] strArr = {"one", "two", "three", "four"};
+		
+		Stream<String> stringStream = Arrays.stream(strArr);
 		
 		ArrayList<Integer> intList = new ArrayList<Integer>();
 		intList.add(1);
@@ -17,16 +23,22 @@ public class CreatingStreams {
 		intList.add(3);
 		intList.add(4);
 		
+		Stream<Integer> intStream = intList.stream();
+		
 		Stack<Random> randStack = new Stack<Random>();
 		randStack.push(new Random());
 		randStack.push(new Random());
 		randStack.push(new Random());
 		randStack.push(new Random());
 		
+		Stream<Random> randStream = randStack.stream();
+		
 		ArrayDeque<Double> dQueue = new ArrayDeque<Double>();
 		dQueue.push(0.0);
 		dQueue.push(1.0);
 		dQueue.push(2.0);
 		dQueue.push(3.0);
+		
+		Stream<Double> doubleStream = dQueue.stream();
 	}
 }
